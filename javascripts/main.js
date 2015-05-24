ae10637eccdc32f0cbe1cf97874d075faa7b13a2
Oliver Robinson <oliverrobinsonsoftware@gmail.com>
2015-05-24 20:06:33 +1200

    Add tabs!

--- javascripts/main.js
+++ javascripts/main.js
@@ -38,3 +38,58 @@ function loadGame(){
 	document.getElementById('cursors').innerHTML = cursors;
 	document.getElementById('cursorCost').innerHTML = Math.floor(10 * Math.pow(1.1,cursors));
 }
+
+var tabLinks = new Array();
+var contentDivs = new Array();
+
+function init() {
+  var tabListItems = document.getElementById('tabs').childNodes;
+  for ( var i = 0; i < tabListItems.length; i++ ) {
+	if ( tabListItems[i].nodeName == "LI" ) {
+	  var tabLink = getFirstChildWithTagName( tabListItems[i], 'A' );
+	  var id = getHash( tabLink.getAttribute('href') );
+	  tabLinks[id] = tabLink;
+	  contentDivs[id] = document.getElementById( id );
+	}
+  }
+
+  var i = 0;
+  for ( var id in tabLinks ) {
+	tabLinks[id].onclick = showTab;
+	tabLinks[id].onfocus = function() { this.blur() };
+	if ( i == 0 ) tabLinks[id].className = 'selected';
+	i++;
+  }
+
+  var i = 0;
+  for ( var id in contentDivs ) {
+	if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
+	i++;
+  }
+}
+
+function showTab() {
+  var selectedId = getHash( this.getAttribute('href') );
+
+  for ( var id in contentDivs ) {
+	if ( id == selectedId ) {
+	  tabLinks[id].className = 'selected';
+	  contentDivs[id].className = 'tabContent';
+	} else {
+	  tabLinks[id].className = '';
+	  contentDivs[id].className = 'tabContent hide';
+	}
+  }
+  return false;  // Stop the browser following the link
+}
+
+function getFirstChildWithTagName( element, tagName ) {
+  for ( var i = 0; i < element.childNodes.length; i++ ) {
+	if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+  }
+}
+
+function getHash( url ) {
+  var hashPos = url.lastIndexOf ( '#' );
+  return url.substring( hashPos + 1 );
+}
